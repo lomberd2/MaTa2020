170350eeca233ce52c6f031a0fe3f4204f32d4fc
lomberd2 <deinemamaistdumm@hotmail.com>
2020-10-02 09:51:39 +0200

    Update Quadratsummen.java
    Denkfehler von mir. Jannik is still doof

--- Trainingslager/Quadratsummen.java
+++ Trainingslager/Quadratsummen.java
@@ -7,10 +7,12 @@ public class Quadratsummen {
     public static void start(){
         System.out.println("Bitte geben sie ihr Ende an: ");
         int endeIndex = IO.readInteger();
-        int ergebniss = 1;
+        double summe = 0;
 
         for(int i = 0;  i <= endeIndex; i++){
-            System.out.println(i + "*" + i + " = " + i * i);
+            summe += i * i;
+            //System.out.println(i + "*" + i + " = " + );
         }
+        System.out.println("Das Ergebniss lautet" + summe);
     }
 }
